8bf22efbfa9231c627e8d064949a1eb45d5b3e4d
Apoorva Kaushik <apoorva.kaushik@gmail.com>
2023-02-10 19:20:35 -0500

    Automatic growth of player

--- Shadow-Game/Assets/Scripts/Player.cs
+++ Shadow-Game/Assets/Scripts/Player.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform segmentPrefab;
+
     private Vector2 _direction;
+    private List<Transform> _segments;
 
     void Start()
     {
-        // Repeat the Move() function every 0.04 seconds 
-        //      -> increase time to make player slower
+        // Repeat the Move() function every 0.04 seconds -> boost time to make slower
         InvokeRepeating("Move", 0.04f, 0.04f);
+
+        _segments = new List<Transform>();
+        _segments.Add(transform);
+        InvokeRepeating("Grow", 2, 2);
     }
 
     // Update is called once per frame
@@ -31,10 +37,22 @@ public class Player : MonoBehaviour
     }
 
     public void Move(){
-        this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x + _direction.x), 
-            Mathf.Round(this.transform.position.y + _direction.y), 
+        // move the segments in reverse order each segment is following the one in front of it
+        for (int i = _segments.Count - 1; i > 0; i--){
+            _segments[i].position = _segments[i - 1].position;
+        }
+        // move the head in the direction of the input
+        transform.position = new Vector3(
+            Mathf.Round(transform.position.x + _direction.x), 
+            Mathf.Round(transform.position.y + _direction.y), 
             0.0f
         );
+    }
+
+    // create a new segment, place it on "tail end" of the shadow, and add it to the list
+    private void Grow(){
+        Transform newSegment = Instantiate(segmentPrefab);
+        newSegment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(newSegment);
     }
 }
