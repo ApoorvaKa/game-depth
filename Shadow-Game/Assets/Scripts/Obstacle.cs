b08331f693459c8bb623c672c928b2b6a7e4a57f
ec3938 <ec3938@nyu.edu>
2023-02-15 10:09:58 -0500

    Added basic obstacles

---
+++ Shadow-Game/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
